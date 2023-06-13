d70a593e9c9795b07b744d750db70ce2d19e6eb3
krishnaThati <130240327+krishnaThati@users.noreply.github.com>
2023-06-13 21:49:20 +0530

    Create IAM.tf

---
+++ IAM.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  
+  region     = "ap-northeast-1"
+  access_key = "AKIAUSA2ZQZ*********"
+  secret_key = "nIs2Xo79lIr845TFh3h8JKP**********"
+}
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_access_key" "lb" {
+  user = aws_iam_user.lb.name
+}
+
+data "aws_iam_policy_document" "lb_ro" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "lb_ro" {
+  name   = "test"
+  user   = aws_iam_user.lb.name
+  policy = data.aws_iam_policy_document.lb_ro.json
+}
